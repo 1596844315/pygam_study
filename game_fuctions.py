1668492c6ecad10dc604f4867631b58dd7b9e70b
xikeso <1596844315@qq.com>
2018-06-10 17:47:34 +0800

    消除多余的子弹

--- game_fuctions.py
+++ game_fuctions.py
@@ -3,16 +3,18 @@ import sys
 import pygame
 from bullet import Bullet
 
-def check_keydown_event(event,ship,setting,screen,bullet):
+def check_keydown_event(event,ship,setting,screen,bullets):
     '''检查按键按下'''
     if event.key == pygame.K_RIGHT:#如果按下右键
         ship.move_right = True
     elif event.key == pygame.K_LEFT:#如果按下左键
         ship.move_left = True
     elif event.key == pygame.K_SPACE:
+        fire_bullet(setting,screen,ship,bullets)
+def fire_bullet(setting,screen,ship,bullets):
+    if len(bullets)< setting.bullet_allowed:
         new_bullet = Bullet(setting,screen,ship)
-        bullet.add(new_bullet)
-
+        bullets.add(new_bullet)
 def check_keyup_event(event,ship):
     '''检查按键松开'''
     if event.key == pygame.K_RIGHT:#如果松开右键
@@ -42,3 +44,11 @@ def update_screen(setting,screen,ship,bullets):
     for bullet in bullets.sprites():
         bullet.draw_bullet()
     pygame.display.flip() #所有图像展示出来
+
+def update_bullets(bullets):
+    '''消除多余子弹'''
+    bullets.update()
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
+
